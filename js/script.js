ff1d8f0c8e24066e654b34ae2808eae99f08b673
wataru <wataru@hyuga.jp>
2023-02-19 15:49:28 +0900

    TOP PC　

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ const headerNavClick = () => {
 
 headerNavBtn[0].onclick = headerNavClick;
 
+const pageTop = document.getElementsByClassName('pageTop')[0];
+let pageTopOn = 0;
+const scrollTop = () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+}
+
+pageTop.addEventListener('click',scrollTop)
+
+window.addEventListener('scroll',function(){
+
+   if(window.pageYOffset > 150 && pageTopOn === 0){
+    pageTopOn = 1;
+    pageTop.animate([{opacity: '0'}, {opacity: '1'}], 500)
+    pageTop.classList.add('active')
+}else if(window.pageYOffset <= 150 && pageTopOn === 1){
+    pageTopOn = 0;
+    pageTop.animate([{opacity: '1'}, {opacity: '0'}], 500)
+    pageTop.classList.remove('active')
+}
+
+});   
---
+++ js/topSlider.jquery.js
@@ -0,0 +1,26 @@
+//Main Image
+$('.slider').slick({
+	fade:false,
+	autoplay:true,
+	arrows:true,
+	autoplaySpeed:6000,
+	speed:1000,
+	pauseOnFocus:false,
+	pauseOnHover:false,
+	pauseOnDotsHover:false,
+	prevArrow:'.top-prev',
+	nextArrow:'.top-next',
+	swipe:true,
+	cssEase:'ease-out',
+	useCSS:true,
+	dots:false,
+	centerMode:true,
+	centerPadding:0
+});
+$('.slick-dots').prependTo('.dots');
+
+$('.slick-slide.slick-current').removeClass('slick-current').addClass('my');
+
+$('.main_img').imagesLoaded(function(){
+	$('.my').addClass('slick-current');
+});
